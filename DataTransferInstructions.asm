ef395bff9d9862031998e872eb9d0bb9a23a0aab
Rob <houghton.robert.e@gmail.com>
2019-07-11 19:57:33 -0600

    Create DataTransferInstructions.asm

---
+++ DataTransferInstructions.asm
@@ -0,0 +1,44 @@
+Let's play a game, 1 incorrect
+when in doubt, say if... 
+cmove ;conditional move equal
+cmovz ;'' zero
+cmovne ;'' not equal
+cmovnz ;'' not zero
+cmova ;'' above
+cmovnbe ;'' not below equal
+cmovae ;'' above equal
+cmovbe ;'' BElow equal
+cmovna ;'' not above
+cmovg ;'' greater
+cmovnle ;'' not less equal 
+cmovge ;'' greater equal
+cmovnl ;'' not less
+cmovl ;'' less
+cmovnge ;''not greater equal
+cmovle ;'' less equal
+cmovng ;'' not greater
+cmovc ;'' carry
+cmovnc ;'' not carry
+cmovo ;''overflow
+cmovno ;''not overflow
+cmovs ;''sign (negative)
+cmovns ;''not sign(negative)
+cmovp ;'' parity
+cmovne ;'' parity even
+cmovnp ;'' not parity
+cmovpo ;'' parity odd.
+xchg ; exchange
+bswap ; byte swap
+xadd ; exchange (and) add
+cmpxchg ; compare exchange
+push 
+pop
+pusha ; push a (?)
+pushad 
+popa
+popad
+cwd ;convert word doubleword
+cdq ;''quadword
+cwd 
+cwde ;convert byte to doubleword (in)eax
+movsx ; move sign extend
